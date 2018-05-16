32e45089023d6ccde906c713a25a67444b6301f2
Adam Joyce <adamjoyce@outlook.com>
2018-05-16 16:09:54 +0100

    Player selection scalable with maxPlayer prop

--- app/components/Players.js
+++ app/components/Players.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import converter from 'number-to-words';
 
 import Categories from './Categories';
 
@@ -11,24 +13,55 @@ class Players extends React.Component {
   }
 
   selectPlayers(playerCount) {
-    console.log('PLAYERS SELECTED');
+    console.log(playerCount + ' player(s) selected');
     this.setState(() => ({playerCount}));
   }
 
   render() {
+    const {maxPlayers} = this.props;
     const {playerCount} = this.state;
+    const playerButtons= [];
+
+    // Setup the player button options.
+    if (!playerCount) {
+      for (let i = 0; i < maxPlayers; i++) {
+        const playerNumber = i + 1;
+
+        // 'Wordify' the number of players and give it a capital.
+        let convertedNumber = converter.toWords(playerNumber);
+        convertedNumber = convertedNumber[0].toUpperCase()
+          + convertedNumber.substring(1);
+
+        playerButtons.push(
+          <button
+            key={playerNumber}
+            onClick={() => this.selectPlayers(playerNumber)}>
+            {/* Deal with singular player instance. */}
+            {convertedNumber !== 'One'
+              ? `${convertedNumber} Players`
+              : `${convertedNumber} Player`}
+          </button>
+        );
+      }
+    }
+
     return(
       !playerCount
         ? <div>
             <h1>How many of you want to play?</h1>
-            <button onClick={() => this.selectPlayers(1)}>One Player</button>
-            <button onClick={() => this.selectPlayers(2)}>Two Players</button>
-            <button onClick={() => this.selectPlayers(3)}>Three Players</button>
-            <button onClick={() => this.selectPlayers(4)}>Four Players</button>
+            {playerButtons}
           </div>
         : <Categories playerCount={playerCount} />
     );
   }
+}
+
+Players.propTypes = {
+  maxPlayers: PropTypes.number.isRequired
+}
+
+Players.defaultProps = {
+  maxPlayers: 4
 }
 
 export default Players;
